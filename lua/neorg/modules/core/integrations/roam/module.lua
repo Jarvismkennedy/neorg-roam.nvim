efee50a10e82c0fad5c16d6ea892853514cc9b74
Jarvismkennedy <77736532+Jarvismkennedy@users.noreply.github.com>
2023-08-19 10:49:53 -0400

    add - search for neorg files. Create new file with metadata if not exist

--- lua/neorg/modules/core/integrations/roam/module.lua
+++ lua/neorg/modules/core/integrations/roam/module.lua
@@ -10,45 +10,35 @@ end
 
 module.private = {
     config = {
-        select_prompt = "<C-c>",
+        select_prompt = "<C-n>",
+        find_note = "<leader>nrf",
     },
-    generate_picker = function()
+    generate_picker = function(files, curr_wksp)
         local pickers = require("telescope.pickers")
         local finders = require("telescope.finders")
         local sorter = require("telescope.sorters")
         local make_entry = require("telescope.make_entry")
         local actions = require("telescope.actions")
         local action_state = require("telescope.actions.state")
-
-        local dirman = module.required["core.dirman"]
-        if dirman == nil then
-            return error("This module requires core.dirman")
-        end
-        local curr_wksp = dirman.get_current_workspace()
-        -- replace this with a sql lite db call later
-        local files = dirman.get_norg_files(curr_wksp[1])
-
         return function(action)
             opts = opts or {}
             return pickers.new(opts, {
                 prompt_title = "colors",
                 attach_mappings = function(prompt_bufnr, map)
-                    map({ "i", "n" }, module.private.config.select_prompt, function()
-                        local current_picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
-                        local prompt = current_picker:_get_prompt()
-                        action(prompt, nil)
-                        actions.close(prompt_bufnr)
-                    end)
                     actions.select_default:replace(function()
                         local current_picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
                         local prompt = current_picker:_get_prompt()
-
                         local selection = action_state.get_selected_entry()
-                        --
                         actions.close(prompt_bufnr)
                         action(prompt, selection)
                     end)
-
+                    -- Maps for creating a new note
+                    map({ "i", "n" }, module.private.config.select_prompt, function()
+                        local current_picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
+                        local prompt = current_picker:_get_prompt()
+                        actions.close(prompt_bufnr)
+                        action(prompt, nil)
+                    end)
                     return true
                 end,
                 sorter = sorter.get_fzy_sorter(opts),
@@ -62,16 +52,32 @@ module.private = {
 }
 module.public = {
     find_note = function()
-        local picker = module.private.generate_picker()
+        local dirman = module.required["core.dirman"]
+        if dirman == nil then
+            return error("This module requires core.dirman")
+        end
+
+        local curr_wksp = dirman.get_current_workspace()
+        local files = dirman.get_norg_files(curr_wksp[1])
+
+        local picker = module.private.generate_picker(files, curr_wksp)
         local action = function(prompt, selection)
-            if selection == nil then
-                if prompt == "" or prompt == nil then
-                    return
-                end
-                vim.print("you choose: " .. prompt)
+            local choice = nil
+            if selection == nil and prompt == nil then
                 return
             end
-            vim.print(selection)
+
+            if selection == nil then
+                choice = curr_wksp[2] .. "/" .. prompt .. ".norg"
+                vim.cmd("e " .. choice)
+                vim.cmd("Neorg inject-metadata")
+            else
+                choice = selection[1]
+                vim.cmd("e " .. choice)
+            end
+        end
+        if picker == nil then
+            return
         end
         picker(action):find()
     end,
@@ -92,6 +98,6 @@ module.public = {
 }
 
 vim.keymap.set("n", "<leader>hrr", ":lua require('dev').reload()<CR>")
-vim.keymap.set("n", "<leader>nrf", module.public.find_note)
+vim.keymap.set("n", module.private.config.find_note, module.public.find_note)
 
 return module
