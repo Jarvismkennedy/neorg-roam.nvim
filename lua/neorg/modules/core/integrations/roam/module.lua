600f43e3125f213a2d451a5410e372b0bbbb3796
Jarvis Kenned <jarvis.k18@gmail.com>
2023-08-30 16:49:40 -0400

    refactor capture into its own module.

--- lua/neorg/modules/core/integrations/roam/module.lua
+++ lua/neorg/modules/core/integrations/roam/module.lua
@@ -2,6 +2,8 @@ local neorg = require("neorg.core")
 local utils = require("neorg.modules.core.utils")
 local module = neorg.modules.create("core.integrations.roam")
 
+-- require sub modules
+
 module.setup = function()
     return {
         success = true,
@@ -9,31 +11,9 @@ module.setup = function()
             "core.keybinds",
             "core.dirman",
             "core.esupports.metagen",
+            "core.integrations.roam.capture",
         },
     }
-end
-
-module.load = function()
-    -- register keybinds
-    local keybinds = module.required["core.keybinds"]
-    keybinds.register_keybinds(module.name, {
-        "insert_link",
-    })
-
-    -- define the keybindings
-    local neorg_callbacks = require("neorg.core.callbacks")
-    neorg_callbacks.on_event("core.keybinds.events.enable_keybinds", function(_, keybinds)
-        keybinds.map_event_to_mode("norg", {
-            n = {
-                { module.config.public.keymaps.insert_link, "core.integrations.roam.insert_link" },
-            },
-        }, { silent = true, noremap = true })
-    end)
-
-    -- set the keybind for pulling up the telescope find note
-    vim.keymap.set("n", module.config.public.keymaps.find_note, module.public.find_note)
-    vim.keymap.set("n", module.config.public.keymaps.capture_note, module.public.capture_note)
-    vim.keymap.set("n", module.config.public.keymaps.capture_index, module.public.capture_index)
 end
 module.config.public = {
     keymaps = {
@@ -46,6 +26,30 @@ module.config.public = {
         capture_save = "<C-w>",
     },
 }
+module.load = function()
+    module.required["core.integrations.roam.capture"].set_keymaps(module.config.public.keymaps)
+    -- register keybinds
+    local keybinds = module.required["core.keybinds"]
+    keybinds.register_keybinds(module.name, {
+        "insert_link",
+    })
+
+    -- define the keybindings
+    local neorg_callbacks = require("neorg.core.callbacks")
+    neorg_callbacks.on_event("core.keybinds.events.enable_keybinds", function(_, keybnds)
+        keybnds.map_event_to_mode("norg", {
+            n = {
+                { module.config.public.keymaps.insert_link, "core.integrations.roam.insert_link" },
+            },
+        }, { silent = true, noremap = true })
+    end)
+
+    -- set the keybind for pulling up the telescope find note
+    vim.keymap.set("n", module.config.public.keymaps.find_note, module.public.find_note)
+    vim.keymap.set("n", module.config.public.keymaps.capture_note, module.public.capture_note)
+    vim.keymap.set("n", module.config.public.keymaps.capture_index, module.public.capture_index)
+end
+
 -- handle events.
 module.on_event = function(event)
     local event_handlers = {
@@ -69,52 +73,6 @@ module.events.subscribed = {
 }
 
 module.private = {
-    register_buffer_for_capture_keymaps = function(buf)
-        vim.keymap.set("n", module.config.public.keymaps.capture_save, module.public.capture_save, { buffer = buf })
-        vim.keymap.set("n", module.config.public.keymaps.capture_cancel, module.public.capture_cancel, { buffer = buf })
-        vim.keymap.set("v", module.config.public.keymaps.capture_save, module.public.capture_save, { buffer = buf })
-        vim.keymap.set("v", module.config.public.keymaps.capture_cancel, module.public.capture_cancel, { buffer = buf })
-        vim.keymap.set("i", module.config.public.keymaps.capture_save, module.public.capture_save, { buffer = buf })
-        vim.keymap.set("i", module.config.public.keymaps.capture_cancel, module.public.capture_cancel, { buffer = buf })
-    end,
-    register_buffer_for_capture_link_keymaps = function(buf)
-        vim.keymap.set(
-            "n",
-            module.config.public.keymaps.capture_save,
-            module.public.capture_link_save,
-            { buffer = buf }
-        )
-        vim.keymap.set(
-            "n",
-            module.config.public.keymaps.capture_cancel,
-            module.public.capture_link_cancel,
-            { buffer = buf }
-        )
-        vim.keymap.set(
-            "v",
-            module.config.public.keymaps.capture_save,
-            module.public.capture_link_save,
-            { buffer = buf }
-        )
-        vim.keymap.set(
-            "v",
-            module.config.public.keymaps.capture_cancel,
-            module.public.capture_link_cancel,
-            { buffer = buf }
-        )
-        vim.keymap.set(
-            "i",
-            module.config.public.keymaps.capture_save,
-            module.public.capture_link_save,
-            { buffer = buf }
-        )
-        vim.keymap.set(
-            "i",
-            module.config.public.keymaps.capture_cancel,
-            module.public.capture_link_cancel,
-            { buffer = buf }
-        )
-    end,
     get_files = function()
         local dirman = module.required["core.dirman"]
         if dirman == nil then
@@ -124,59 +82,6 @@ module.private = {
         local curr_wksp = dirman.get_current_workspace()
         local files = dirman.get_norg_files(curr_wksp[1])
         return { curr_wksp, files }
-    end,
-    capture_note = function(file)
-        local buf_win = utils.create_capture_window()
-        local buf = buf_win[1]
-        vim.api.nvim_buf_call(buf, function()
-            -- edit the choice in the capture window, update/inject metadata, jump to bottom
-            -- of file, and enter a new line.
-            vim.cmd("e " .. file)
-            module.private.capture_buffer = vim.api.nvim_win_get_buf(buf_win[2])
-            -- put cursor at the end of metadata.
-            local metadata_present =
-                module.required["core.esupports.metagen"].is_metadata_present(module.private.capture_buffer)
-
-            if metadata_present then
-                vim.cmd("Neorg update-metadata")
-            else
-                vim.cmd("Neorg inject-metadata")
-            end
-            -- search for the end of the metadata tag.
-            local row_of_meta_end = vim.fn.search("@end")
-            vim.cmd(string.format(":call cursor(%d,0)", row_of_meta_end))
-            vim.cmd(":normal o")
-            vim.cmd(":normal o")
-        end)
-        module.private.register_buffer_for_capture_keymaps(vim.api.nvim_win_get_buf(buf_win[2]))
-    end,
-    capture_link = function(file, link)
-        local buf = vim.api.nvim_get_current_buf()
-        local win = vim.api.nvim_get_current_win()
-        local pos = vim.api.nvim_win_get_cursor(win)
-        -- save the buffer and cursor position to insert link on save
-        module.private.capture_link_buffer = { id = buf, row = pos[1], col = pos[2], link = link }
-        local buf_win = utils.create_capture_window()
-        vim.api.nvim_buf_call(buf_win[1], function()
-            -- edit the choice in the capture window, update/inject metadata, jump to bottom
-            -- of file, and enter a new line.
-            vim.cmd("e " .. file)
-            -- put cursor at the end of metadata.
-            local metadata_present =
-                module.required["core.esupports.metagen"].is_metadata_present(module.private.capture_buffer)
-
-            if metadata_present then
-                vim.cmd("Neorg update-metadata")
-            else
-                vim.cmd("Neorg inject-metadata")
-            end
-            -- search for the end of the metadata tag.
-            local row_of_meta_end = vim.fn.search("@end")
-            vim.cmd(string.format(":call cursor(%d,0)", row_of_meta_end))
-            vim.cmd(":normal o")
-            vim.cmd(":normal o")
-        end)
-        module.private.register_buffer_for_capture_link_keymaps(vim.api.nvim_win_get_buf(buf_win[2]))
     end,
 }
 module.public = {
@@ -229,7 +134,7 @@ module.public = {
             else
                 choice = selection[1]
             end
-            module.private.capture_note(choice)
+            module.required["core.integrations.roam.capture"].capture_note(choice)
         end
         if picker == nil then
             return
@@ -238,37 +143,8 @@ module.public = {
     end,
     capture_index = function()
         local curr_wksp = module.required["core.dirman"].get_current_workspace()[2]
-        module.private.capture_note(curr_wksp .. "/index.norg")
+        module.required["core.integrations.roam.capture"].capture_note(curr_wksp .. "/index.norg")
     end,
-    -- capture to workspace index.
-    capture_save = function()
-        vim.api.nvim_buf_call(0, function()
-            vim.cmd("w")
-            vim.cmd("bd")
-        end)
-        vim.api.nvim_input("<esc>")
-    end,
-    capture_cancel = function()
-        vim.api.nvim_buf_call(0, function()
-            vim.cmd("bd!")
-        end)
-        vim.api.nvim_input("<esc>")
-    end,
-    capture_link_save = function()
-        module.public.capture_save()
-        local capture_link = module.private.capture_link_buffer
-        if capture_link == nil then
-            error("Failed to insert link properly: capture_link_buffer is nil")
-        end
-        vim.api.nvim_set_current_buf(capture_link.id)
-        vim.cmd(string.format(":call cursor(%d,%d)", capture_link.row, capture_link.col))
-        vim.api.nvim_put({ capture_link.link }, "c", true, true)
-    end,
-    capture_link_cancel = function()
-        module.private.capture_link_buffer = nil
-        module.public.capture_cancel()
-    end,
-    --
 
     insert_link = function()
         local wksp_files = module.private.get_files()
@@ -283,7 +159,10 @@ module.public = {
             local link = ""
             if file == nil then
                 link = "{:" .. prompt .. ":}[" .. prompt .. "]"
-                module.private.capture_link(curr_wksp[2] .. "/" .. prompt .. ".norg", link)
+                module.required["core.integrations.roam.capture"].capture_link(
+                    curr_wksp[2] .. "/" .. prompt .. ".norg",
+                    link
+                )
             else
                 local start_index = #curr_wksp[2] + 2
                 link = "{:" .. file[1]:sub(start_index, -6) .. ":}[" .. file[1]:sub(start_index, -6) .. "]"
